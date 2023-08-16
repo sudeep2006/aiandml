d6ab637075139717c9ad537b22dbdeeb90112082
sudeep2006 <tmsudeep55@gmail.com>
2023-08-16 10:54:27 +0530

    change

---
+++ dataset.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+
+data=pd.read_csv("C:/Users/SPTINT-28/Desktop/dataset/pubg.csv")
+print(data)
+print(data.head(5))
+print(data.tail(5))
+print(data.info())
+print(data.dtypes)
+print(data.count())
+
+
+
